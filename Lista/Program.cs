15f8dd467863754f11f7c453af86e5fe884f1fa3
armandoj-oliveira <ajvdoliveira678512@gmail.com>
2024-01-03 14:37:55 -0300

    implementacao e correcao de codigo

--- Lista/Program.cs
+++ Lista/Program.cs
@@ -1,10 +1,11 @@
-﻿List<string> liguagenspogramacao = ["C#", "Java", "JavaScript"];
+﻿List<string> liguagensPogramacao = ["C#", "Java", "JavaScript"];
+List<int> somaInteiro = new List<int>();
 
 void MostrarLista()
 {
     Console.WriteLine("A Lista contém os seguintes itens:");
 
-    foreach (string l in liguagenspogramacao)
+    foreach (string l in liguagensPogramacao)
     {
 
         Console.WriteLine(l);
@@ -19,7 +20,7 @@ void CompararItemLista()
 
     bool encontrado = false;
 
-    foreach(string item in liguagenspogramacao)
+    foreach(string item in liguagensPogramacao)
     {
         if (item.ToLower() == escolhaLinguagem.ToLower())
         {
@@ -40,18 +41,142 @@ void EncontrarPorPosição()
     Console.Write("Digita um número (1 a 3): ");
     int numEscolha = Convert.ToInt32(Console.ReadLine());
 
-    if( numEscolha == 0 || numEscolha > liguagenspogramacao.Count())
+    if( numEscolha == 0 || numEscolha > liguagensPogramacao.Count())
     {
         Console.WriteLine("Número inválido para busca");
     } else
     {
-        string elemento = liguagenspogramacao[numEscolha - 1];
+        string elemento = liguagensPogramacao[numEscolha - 1];
         Console.WriteLine($"O item escolhido foi {elemento}!");
     }
 }
 
-CompararItemLista();
-Console.WriteLine("");
-EncontrarPorPosição();
-Console.WriteLine("");
-MostrarLista();
+void MostarNumeroLista()
+{
+    if(somaInteiro.Count == 0)
+    {
+        Console.WriteLine("Não existe nenhum item na lista");
+    } else
+    {
+        Console.WriteLine("A lista contém o(s) seguinte(s) número(s):");
+        foreach (int num in somaInteiro)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
+void AdicionarNumeroLista()
+{
+    bool continuar = false;
+
+    while(!continuar)
+    { 
+        Console.Write("Informe o número que deseja inserir: ");
+        int numDesejado = Convert.ToInt32(Console.ReadLine()!);
+        somaInteiro.Add(numDesejado);
+        Console.Write("Deseja adicionar outro? (S/N): ");
+        char adcNum = Convert.ToChar(Console.ReadLine()!.ToLower());
+
+        if(adcNum == 'n')
+        {
+            continuar = true;
+        }
+    }
+}
+
+void SomarNumerosLista()
+{
+    if(somaInteiro.Count == 0)
+    {
+        Console.WriteLine("Adicione pelo menos dois números antes de usar esta opção!!!");
+    } else
+    {
+
+        int totSoma = 0;
+
+        foreach (int numero in somaInteiro)
+        {
+            totSoma += numero;
+        }
+
+        Console.WriteLine($"A soma dos números da lista é {totSoma}");
+    }
+}
+
+void ExecutarPrograma()
+{
+    bool sair = false;
+
+    while (!sair)
+    {
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine("Escolha alguma das opções a seguir!");
+        Console.ResetColor();
+        Console.WriteLine("\n***********************************************************************************");
+        Console.WriteLine("Digite 1 para mostrar uma lista programada de Bandas!");
+        Console.WriteLine("Digite 2 para encontrar a posição de um elemento de uma lista programada de Bandas!");
+        Console.WriteLine("Digite 3 para comparar um item de uma lista programada de Bandas!");
+        Console.WriteLine("Digite 4 para adicionar números em uma lista!");
+        Console.WriteLine("Digite 5 para mostrar os números da lista!");
+        Console.WriteLine("Digite 6 para somar todos o números da lista!");
+        Console.WriteLine("Digite 0 para sair!");
+        Console.WriteLine("***********************************************************************************");
+        Console.Write("\n--> ");
+        int escolha = Convert.ToInt32(Console.ReadLine()!);
+
+        switch (escolha)
+        {
+            case 1:
+                Console.Clear();
+                MostrarLista();
+                Console.Write("\nAGUARDE 3 SEGUNDOS, NÃO FAÇA NADA!!!");
+                Thread.Sleep(3000);
+                break;
+            case 2:
+                Console.Clear();
+                EncontrarPorPosição();
+                Console.Write("\nAGUARDE 3 SEGUNDOS, NÃO FAÇA NADA!!!");
+                Thread.Sleep(3000);
+                break;
+            case 3:
+                Console.Clear(); 
+                CompararItemLista();
+                Console.Write("\nAGUARDE 3 SEGUNDOS, NÃO FAÇA NADA!!!");
+                Thread.Sleep(3000);
+                break;
+            case 4:
+                Console.Clear();
+                AdicionarNumeroLista();
+                Console.Write("\nAGUARDE 3 SEGUNDOS, NÃO FAÇA NADA!!!");
+                Thread.Sleep(3000);
+                break;
+            case 5:
+                Console.Clear();
+                MostarNumeroLista();
+                Console.Write("\nAGUARDE 3 SEGUNDOS, NÃO FAÇA NADA!!!");
+                Thread.Sleep(3000);
+                break;
+            case 6:
+                Console.Clear();
+                SomarNumerosLista();
+                Console.Write("\nAGUARDE 3 SEGUNDOS, NÃO FAÇA NADA!!!");
+                Thread.Sleep(3000);
+                break;
+            case 0:
+                Console.Clear();
+                Console.Write("Até uma próxima vez!!!");
+                sair = true;
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine("Opção Inválida!!!");
+                Console.Write("\nAGUARDE 3 SEGUNDOS, NÃO FAÇA NADA!!!");
+                Thread.Sleep(3000);
+                break;
+        }
+    }
+}
+
+ExecutarPrograma();
